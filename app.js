959b124d1bbf25bb46211dadfee6b85ca34f8f0d
gvillablanca <g.villalobo.m@gmail.com>
2018-01-20 16:38:12 -0300

    reidentacion y correccion del ejercicio

--- app.js
+++ app.js
@@ -1,71 +1,60 @@
-//funcion principal
-function main (){
-	do{
-		var respuesta = prompt("Desea: 1:codificar - 2:decodificar");//opciones
-		if(respuesta!= ""){
-			if(respuesta == "1"){//si es 1 cifrar
-				cipher();//llamar funcion cifrar
-			}else if(respuesta == "2"){//si es 2 decifrar
-				decipher();//llamar funcion decifrar
-			}else{
-				alert("ingresar opcion valida");//solicitar opcion correcta
-			}
-		}
-	}while (respuesta == "" || (respuesta != "1" && respuesta != "2"));//valido que la respuesta sea una de las opciones
+function main() {
+  do {
+    var answer = prompt('Desea: \n\ 1:Codificar \n\ 2:Decodificar'); //  primero pregunto que es lo que quiere hacer el usuario
+    if (answer !== '') {
+      if (answer === 1 || answer === 2) {
+        var string = phrase(); //  valor que se obtiene de la funcion phrase
+        if (answer === '1') { //  si su opcion es 1 accedemos a la funcion cipher
+          return document.write(cipher(string));
+        } else if (answer === '2') { //  si su opcion es 2 accedemos a la funcion decipher
+          return document.write(decipher(string));
+        }
+      } else {
+        alert('Por favor ingrese una opcion valida');
+      }
+    }
+  } while (answer === '' || (answer !== '1' && answer !== '2'));
 }
 
-//funcion cifrar
-function cipher(str){
-	var temp = []; //variable donde guardare lo que estoy codificando
-	var temp2 = []; //donde ire guardando los valores de las formulas
-	var strcode = [];//donde pondre mi letras codificadas ya
-	do{ //do while para verificar que el texto entregado sea solo string
-		var str = prompt("Ingrese texto a codificar");//solicitar cadena
-			for(var i =0; i< str.length; i++){ //recorro mi texto
-				if(typeof(str) == String){//paso a ascii letra por letra
-					str.charCodeAt(i);
-					temp.push(i);//los guardo en mi array temp codificados a ascii
-				}
-				for(var i = 0; i < temp.length; i++){ //recorro mi array con la palabra ascii
-					if(typeof(temp[i] == Number)){//verifico que esten es ascii
-						temp2[i] = (temp[i] - 65 + 33) % 26 + 26; //aplico la formula acii y las guardo en mi var temporal 2
-					}for(var i = 0; i< temp2.length; i++){//recorro mi variable temp2 con la formula ya aplicada
-						temp2[i] = (temp2[i] + 33) % 26; //aplico la formula de cesar
-						String.fromCharCode(i);//los vuelvo a letras
-						strcode.push(i);// los voy metiendo en donde estara mi resultado
-						strcode.join();//lo uno todo para que quede una cadena
-					}return strcode;//retorno mi nueva cadena
-				}
-			}
-	}while (str == "" || typeof(str) == String || str == " ");//valido que la cadena a evaluar sea string
+function phrase() {
+  do {
+    var phraseAnswer = prompt('Ingrese su frase');
+  } while (phraseAnswer === '' || containsNum(phraseAnswer) === true || isNaN(phraseAnswer) === false); //  condicion para detener la funcion
+  return phraseAnswer; //  el valor de esta frase se guarda en string de la funcion main y sera el valor que entrara a las funciones cipher y decipher
 }
 
-//funcion decifrar
-function decipher(str){
-	var temp = []; //variable donde guardare lo que estoy decodificando
-	var temp2 = []; //donde ire guardando los valores de las formulas
-	var strdecode = [];//donde pondre mi letras decodificadas ya
-	do{
-		var str = prompt("Ingrese texto a decodificar");//recibo cadena codificada
-		for(var i =0; i< str.length; i++){ //recorro mi texto
-				if(typeof(str) == String){//paso a ascii letra por letra
-					str.charCodeAt(i);
-					temp.push(i);//los guardo en mi array temp codificados a ascii
-				}
-				for(var i = 0; i < temp.length; i++){ //recorro mi array con la palabra ascii
-					if(typeof(temp[i] == Number)){//verifico que esten es ascii
-						temp2[i] = (temp[i] - 65 + 33) % 26 + 26; //aplico la formula acii y las guardo en mi var temporal 2
-					}for(var i = 0; i< temp2.length; i++){//recorro mi variable temp2 con la formula ya aplicada
-						temp2[i] = (temp2[i] - 33) % 26; //aplico la formula de cesar
-						String.fromCharCode(i);//los vuelvo a letras
-						strdecode.push(i);// los voy metiendo en donde estara mi resultado
-						strdecode.join();//lo uno todo para que quede una cadena
-					}return strcode;//retorno mi nueva cadena
-				}			
-			}
-
-	}while (str == "" || typeof(str) == String || str == " ");//valido que la cadena a evaluar sea string
+function cipher(string) {
+  var output = ''; // variable de salida sera en blanco
+  for (var i = 0 ; i < string.length; i++) {
+    var character = string[i]; //  guardamos el caracter en una variable
+    if (character.match(/[a-z]/i)) { //  funcion regular para verificar si el caracter es una letra
+      var ascii = string.charCodeAt(i); //  obtener codigo ascii
+      if ((ascii >= 65) && (ascii <= 90)) { //  rango de las mayusculas
+        character = String.fromCharCode(((ascii - 65 + 33) % 26) + 65); //  le sumamos 33 porque debemos avanzar
+      } else if ((ascii >= 97) && (ascii <= 122)) { //  rango de las minusculas
+        character = String.fromCharCode(((ascii - 97 + 33) % 26) + 97); //  lo mismo aqui, sumamos 33 para avanzar
+      }
+      output += character; //  a nuestra variable de salida le concatenamos cada caracter
+    }
+    return output; //  retornamos el string
+  }
 }
 
+function decipher(string) {
+  var output = ''; // variable de salida sera en blanco
+  for (var i = 0 ; i < string.length; i++) {
+    var character = string[i]; // gurdamos el caracter en una variable
+    if (character.match(/[a-z]/i)) { //  funcion regular para verificar si el caracter es una letra
+      var ascii = string.charCodeAt(i); //  obtener codigo ascii
+      if ((ascii >= 65) && (ascii <= 90)) { //  rango de las mayusculas
+        character = String.fromCharCode(((ascii - 65 - 33) % 26) + 65); // ahora la formula es menos porque debemos devolvernos
+      } else if ((ascii >= 97) && (ascii <= 122)) { //  rango de las minusculas
+        character = String.fromCharCode(((ascii - 97 - 33) % 26) + 97); // debemos devolvernos por eso es menos 33 no mas 33
+      }
+      output += character; //  a nuestra variable de salida le concatenamos cada caracter
+    }
+    return output; //  retornamos el string
+  }
+}
 
-main();//llamo mi funcion principal inmediatamente
+main();
